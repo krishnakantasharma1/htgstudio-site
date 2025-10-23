fda6d15329ff7f9b6c9d6cfd1d99d012edfba9db
krishnakantasharma1 <krishnakantasharma297@gmail.com>
2025-10-23 22:48:49 +0530

    Your message

--- app/api/create-order/route.js
+++ app/api/create-order/route.js
@@ -9,7 +9,7 @@ export async function POST(req) {
     }
 
     const instance = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
+  key_id: rzp_live_RW88YpfthOTT67,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
